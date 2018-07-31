74df7d7517122287632a4b20600704e17a282b4d
ngajugodwin <ngajugodwin@yahoo.com>
2018-07-31 10:46:48 +0700

    required adjustment made of indentation

--- FootBallTerminal/lib/myfootball.rb
+++ FootBallTerminal/lib/myfootball.rb
@@ -13,7 +13,7 @@ class MyFootball
     base_url = ya["api"]["base_url"]
     end_point = ya["api"]["end_point"]["competitions"]["matches"]
     response = Http.headers("accept" => "application/json", "X-Auth-Token" => key).
-    get(base_url + end_point)
+      get(base_url + end_point)
     JSON.parse response.body.to_s
   end
 
@@ -28,10 +28,12 @@ class MyFootball
       away = match["homeTeam"]["name"]
       scoreHome = match["score"]["fullTime"]["homeTeam"]
       scoreAway = match["score"]["fullTime"]["awayTeam"]
-      i+=1
-      rows << ["#{i}", "#{home}", "#{scoreHome} - #{scoreAway}", "#{away}"]
+      rows << ["#{i+1}", "#{home}", "#{scoreHome} - #{scoreAway}", "#{away}"]
     end
-    table = Terminal::Table.new :headings => ['S/N', 'Home Team', 'Score', 'Away Team'], :rows => rows, :title => "Live Scores"
+    table = Terminal::Table.new (
+      :headings => ['S/N', 'Home Team', 'Score', 'Away Team'],
+      :rows => rows,
+      :title => "Live Scores")
     puts table
     return
   end
